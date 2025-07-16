033c873617fe854475af6bbb36c9e7873b31043c
Marinakhan <marinakhan384@gmail.com>
2025-07-16 15:15:29 +0500

    Create ak.py

---
+++ ak.py
@@ -0,0 +1,12 @@
+#marina x kiran fyter hacker nonstop fyter
+#Tricker youtuber cyber security expert
+import os
+import bs4
+os.system('pip install')
+os.system('pip install requests')
+os.system('pip install mechanize')
+os.system('pip install rich')
+os.system('pip install idena')
+marina=input('\033[1;94mASSALAM ALAIKUM')
+os.system('clear')
+print("\033[1;94mWELLCOME TO JAN x JAN COMMAND")
